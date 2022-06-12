6889cdc54aaaf2ceeb2ead0816bcef6ba05950be
SOBANBABUBIITK <sobanbabubiitk@gmail.com>
2022-06-12 14:56:35 +0530

    Packages to install

---
+++ my-install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install ubuntu-restricted-extras
+sudo apt-get install emacs
+sudo apt-get install texlive-full
+sudo apt-get install textstudio
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install build-essential
+sudo apt-get install laptop-mode-tools
+sudo apt install gnome-tweaks
